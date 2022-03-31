4bc288d42249fadc40b6016a60188c9a3277dd96
Duy <v.hbaoduy@gmail.com>
2022-03-31 18:07:30 +0700

    clone code and delete uneccesary files

---
+++ datasets/__init__.py
@@ -0,0 +1,17 @@
+
+from .market import *
+from .image_dataset import *
+
+__factory = {
+    'market1501': Market1501,
+}
+
+
+def get_names():
+    return __factory.keys()
+
+
+def init_dataset(name, *args, **kwargs):
+    if name not in __factory.keys():
+        raise KeyError("Unknown datasets: {}".format(name))
+    return __factory[name](*args, **kwargs)
